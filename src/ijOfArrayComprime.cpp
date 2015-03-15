0ff6d4bb31845aa32df0b441863628ef7506c5ae
ring04w <ring04w@gmail.com>
2015-03-15 18:52:28 +0800

    Create an bool array a[N][N] which consisted of 2 Comprimed integers

--- src/ijOfArrayComprime.cpp
+++ src/ijOfArrayComprime.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 
+const int N = 4;
 
 int gcd(int m, int n)
 {
@@ -22,35 +23,58 @@ int gcd(int m, int n)
 	return n;
 }
 
-void isPrime(int m, int n)
+int isPrime(int m, int n)
 {
-	if(gcd(m, n) == 1)
-		return true;
+	if(gcd(m, n) > 1)
+		return 0;
 	else
-		return -1;
+		return 1;
 
 }
-/***********
-void ijOfArrayComprime(int a[], int M, int N)
+
+void ijOfArrayComprime(int a[N][N], int N)
 {
 	int i;
 	int j;
-	for(i = 0; i < M; ++i)
+	for(i = 0; i < N; ++i)
 		for(j = 0; j < N; ++j)
 		{
-			if
+			if(isPrime(i, j))
+				a[i][j] = 1;
+			else
+				a[i][j] = 0;
 		}
 }
-*************/
+
+
 
 int main(int argc, char const *argv[])
 {
+	/***********
 	int isM;
 	isM = isPrime(16, 24);
 	if(isM)
-		cout<<"16 and 24 is comprime"<<endl;
+		cout<<"16 and 24 is  comprime"<<endl;
 	else
-		cout<<"16 and 24 is not comprime"<<endl;
+		cout<<"16 and 24 is not  comprime"<<endl;
+*********/
+	//int M;
+	//int N;
+	int i;
+	int j;
+	int a[N][N];
+//	cout<<"Please input the line and row of the array"<<endl<<">>>";
+//	cin>>M>>N;
+	ijOfArrayComprime(a, N);
+	
+	for(i = 0; i < N; ++i)
+		for(j = 0; j < N; ++j)
+			{
+				cout<<a[i][j];
+				cout<<endl;
+			}
+
+
 
 	return 0;
 }
