b9487622159ab343ea27a765a106bdce726ff905
ring04w <ring04w@gmail.com>
2015-03-19 09:31:51 +0800

    counting the time which program cost totally

---
+++ src/ProgramTimeCost.cpp
@@ -0,0 +1,27 @@
+// Source : Algorithms 4th Edition
+//Robert Sedgewick and Kevin Wayne
+// Author : ring04w
+// Date   : 2015-03-19
+
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int int main(int argc, char const *argv[])
+{
+	clock_t startTime;
+	clock_t finishTime;
+	double totalTime;
+	startTime = clock();
+
+	/********code******/
+	finishTime = clock();
+
+	totalTime = finishTime - startTime;
+	cout<<totalTime<<endl;
+	
+
+
+	return 0;
+}
