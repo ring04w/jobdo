2c52ffaad16ec288f927212accef36e8e8368870
ring04w <ring04w@gmail.com>
2015-07-02 20:38:40 +0800

    Search element in yangshi matrix

---
+++ src/SearchOfYangShiMatrix.java
@@ -0,0 +1,17 @@
+public class SearchOfYangShiMatrix{
+	public bool searchMatrix(vector<vector><int> &matrix, int target){
+		int m = matrix.size();
+		int n = matrix[0].size();
+
+		for (int r = 0, c = n-1; r < m && c >0){
+			if (matrix[r][c] > target){
+				--c;
+			}else if (matrix[r][c] < target){
+				++r;
+			}else{
+				return true;
+			}
+		}
+		return false;
+	}
+}
