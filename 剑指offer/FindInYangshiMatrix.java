228ed29651c4aa08262bf7193ad608dd0ad723ed
ring04w <ring04w@gmail.com>
2015-08-08 16:58:43 +0800

    在杨氏矩阵中查找制定数

---
+++ 剑指offer/FindInYangshiMatrix.java
@@ -0,0 +1,18 @@
+
+class Solution{
+	public boolean findInYangshiMatrix(int[][] &array, int target){
+		int m = array.length;
+		int n = array[0].length;
+
+		for (int r = 0, s = n - 1; r < m && s > 0;) {
+			if(array[r][s] > target){
+				s--;
+			}else if(array[r][s] < target){
+				r++;
+			}else {
+				return true;
+			}
+		}
+		return false;
+	}
+}
