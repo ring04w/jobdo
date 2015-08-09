495726f0cb35372b4edcb41fecabf944b8e37f7a
ring04w <ring04w@gmail.com>
2015-08-09 16:59:01 +0800

    替换字符串中空格，从后往前把字符复制到新字符串，遇到空格用％20代替

---
+++ 剑指offer/ReplaceSpace.java
@@ -0,0 +1,29 @@
+class Solution{
+	public void replaceSpace(char[] str, int length){
+		if(str == null || length == 0)
+		return ;
+
+		int bankSpace = 0;
+		for(int i = 0; i < length; ++i){
+			if(str[i] == ''){
+				bankSpace++;
+			}
+		} 
+
+		int newLength = length + 2 * bankSpace;
+		
+		for(int j = length - 1, i = newLength - 1; j >= 0, i >=0;j--, i--){
+			if(str[j] == ''){
+					str[i] = '0';
+					str[i-1] = '2';
+					str[i-2] = '%';
+					j = j - 1;
+					i = i - 3;
+				}else{
+					str[i] = str[j];
+				}
+				if(i == j)
+					return ;
+			}
+		}
+}
