eedc871e960f54fdc1c61a70781b27d4c979a42f
ring04w <ring04w@gmail.com>
2015-08-10 15:59:00 +0800

    打印倒数第k个结点

--- 剑指offer/KthNodeFromTail.java
+++ 剑指offer/KthNodeFromTail.java
@@ -1,12 +1,20 @@
 
 class Solution{
 	static void printKthNodeFromTail(ListNode *pHead, int k){
-		if(pHead == null)
+		if(pHead == null || k == 0)//空指针访问，崩溃
 			return ;
 		else if(pHead != null && pHead.next == null && k == 1)
 			return pHead;
 
 		ListNode *pNode = pHead;
+		int length = 0;
+		while(pHead.next != 0){
+			length ++;
+		}
+		if(length < k)//链表长度小于k，遇到空指针，崩溃
+			return ;
+
+
 		for(int i = 0; i < k; i++){
 			pNode = pNode.next;
 		}
